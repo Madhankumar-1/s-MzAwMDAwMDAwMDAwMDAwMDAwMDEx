21dd3a1316e4a5af84e90c9b7fc036793dced01d
Madhan Kumar Tachyon Systems <99461480+Madhankumar-1@users.noreply.github.com>
2024-10-22 20:28:11 +0530

    Update npmInstall.sh

--- lambda/npmInstall.sh
+++ lambda/npmInstall.sh
@@ -3,11 +3,16 @@
 # Iterate through all directories in the current directory
 for dir in */; do
     # Check if the directory contains 'src'
-    if [[  -d "$dir/src" ]]; then
-     
-        # Navigate to the 'src' directory and run 'npm i'
-        echo "Running npm i in $dir/src"
-        (cd "$dir/src" && npm i)
+    if [[ -d "$dir/src" ]]; then
+        # Check if .cny_npmrc exists in the 'src' directory
+        if [[ -f "$dir/src/.cny_npmrc" ]]; then
+            echo "Renaming .cny_npmrc to .npmrc in $dir/src"
+            mv "$dir/src/.cny_npmrc" "$dir/src/.npmrc"
+        fi
+        
+        # Navigate to the 'src' directory and run 'npm install'
+        echo "Running npm install in $dir/src"
+        (cd "$dir/src" && npm install)
     else
         echo "Skipping $dir: 'src' folder not found"
     fi
